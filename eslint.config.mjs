a821e1c197621dbbb0f108f75d17184bc1edd980
DiMaacUI <hello.dimaac@gmail.com>
2025-09-18 23:17:00 +0530

    Fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "react-hooks/exhaustive-deps": "off",
       "@next/next/no-img-element": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
